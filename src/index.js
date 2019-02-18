188f02b3c3be485bec01489932d70b1e04f948ac
SiarheiZ <zykau_s@mail.ru>
2019-02-18 15:50:05 +0300

    fix: fix folders structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './js/redux/store';
-import { App } from './js/components/containers';
+import { App } from './js/containers';
 
 import './style/style.scss';
 
---
+++ src/js/containers/App/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => <div className='app'>Hello world</div>;
+
+export default App;
