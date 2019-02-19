329c7d8cb5e7ae72ab3638bba9fe7251a49bad02
SiarheiZ <zykau_s@mail.ru>
2019-02-19 17:32:40 +0300

    fix: fix quotes bugs

--- src/js/components/FavoritesPage/FavoritesPage.js
+++ src/js/components/FavoritesPage/FavoritesPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './FavoritesPage.scss';
 
 const FavoritesPage = () => (
-  <div className="page favorites-page">this is a Favorites page</div>
+  <div className='page favorites-page'>this is a Favorites page</div>
 );
 
 export default FavoritesPage;
--- src/js/components/HomePage/HomePage.js
+++ src/js/components/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './HomePage.scss';
 
 const HomePage = () => (
-  <div className="page home-page">this is a Home page</div>
+  <div className='page home-page'>this is a Home page</div>
 );
 
 export default HomePage;
